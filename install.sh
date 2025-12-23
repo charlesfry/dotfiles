479ab96bec97cd04c3ab7ad506c81dc129c87e5d
Charles Fry <charlesmfry@gmail.com>
2025-12-23 11:19:25 -0500

    init

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r hypr $HOME/.config/hypr
+cp -r nvim $HOME/.config/nvim
+cp -r shell $HOME/.config/shell
+cp -r waybar $HOME/.config/waybar
---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/char/.config/omarchy/current/theme/neovim.lua
